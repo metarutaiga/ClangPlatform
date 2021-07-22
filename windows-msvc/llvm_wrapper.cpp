d885c5bfc9ee0a9d8d88c9400d0802b315a4225c
TAiGA <>
2021-07-23 00:13:46 +0800

    Update

--- windows-msvc/llvm_wrapper.cpp
+++ windows-msvc/llvm_wrapper.cpp
@@ -55,12 +55,15 @@ extern "C" void WINAPI __dyn_tls_init(PVOID, DWORD dwReason, LPVOID)
 #pragma comment(linker, "/alternatename:__CxxThrowException@8=__CxxThrowException")
 #pragma comment(linker, "/alternatename:_atexit=__crt_atexit")
 #pragma comment(linker, "/alternatename:_setjmp=__setjmp3")
+#pragma comment(linker, "/alternatename:___uncaught_exceptions=___uncaught_exception")
 #elif defined(_M_ARM)
 #pragma comment(linker, "/alternatename:atexit=_crt_atexit")
 #pragma comment(linker, "/alternatename:setjmp=__intrinsic_setjmp")
+#pragma comment(linker, "/alternatename:__uncaught_exceptions=__uncaught_exception")
 #elif defined(_M_AMD64) || defined(_M_ARM64)
 #pragma comment(linker, "/alternatename:atexit=_crt_atexit")
 #pragma comment(linker, "/alternatename:setjmp=__intrinsic_setjmpex")
+#pragma comment(linker, "/alternatename:__uncaught_exceptions=__uncaught_exception")
 #endif
 //------------------------------------------------------------------------------
 #ifndef _NO_DLLMAIN_
